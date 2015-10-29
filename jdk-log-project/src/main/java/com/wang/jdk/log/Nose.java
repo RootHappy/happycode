d46c2a62e34722071190adf7d9cf3200dc5d7289
wangyunlong <yunlong.wang@travelzen.com>
2015-10-29 13:06:40 +0800

    add jdk-log-project

---
+++ jdk-log-project/src/main/java/com/wang/jdk/log/Nose.java
@@ -0,0 +1,15 @@
+package com.wang.jdk.log;
+
+import java.util.logging.Logger;
+
+public class Nose {
+
+	private static Logger logger = Logger.getLogger("com.wang.jdk.log.Nose");
+
+	public static void main(String[] args) {
+//		logger.fine("doing stuff");
+		logger.warning("wangyunlong");
+		logger.info("张三");
+	}
+
+}
---
+++ jdk-log-project/src/main/java/com/wang/jdk/log/TestLogger.java
@@ -0,0 +1,30 @@
+package com.wang.jdk.log;
+
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TestLogger {
+
+	public static void main(String[] args) throws SecurityException, IOException {
+		Logger logger = Logger.getLogger("com.wang.jdk.log.TestLogger");
+		ConsoleHandler handler = new ConsoleHandler();
+		handler.setLevel(Level.ALL);
+		logger.addHandler(handler);
+		FileHandler fileHandler = new FileHandler("/home/wangyunlong/log.txt");
+		fileHandler.setLevel(Level.ALL);
+		logger.addHandler(fileHandler);
+		logger.severe("wangyunlong");
+		logger.warning("wangyunong");
+		logger.info("wangyunlong");
+		logger.config("wangyunlong");
+		logger.fine("wangyunlong");
+		logger.finer("wangyunlong");
+		logger.finest("finest");
+
+
+	}
+
+}
